4b0f5f4fbe86e1bfeb41b5f88a3741dc1e74f8f1
Luillin A. Escobar <luillin.escobar@hotmail.com>
2021-09-02 11:25:04 -0500

    Agrego header y footer pdte modificacion

--- js/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $("#exampleModal").on("show.bs.modal", (event) => {
-    var button = $(event.relatedTarget);
-    var modal = $(this);
-    // Use above variables to manipulate the DOM
-  });
-});
---
+++ views/shared/header.php
@@ -0,0 +1,83 @@
+<div class="site-mobile-menu site-navbar-target">
+    <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+    </div>
+    <div class="site-mobile-menu-body"></div>
+</div>
+<div class="top-bar">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <a href="#" class=""><span class="mr-2  icon-envelope-open-o"></span> <span class="d-none d-md-inline-block">info@windowssolutions.somme.com</span></a>
+                <span class="mx-md-2 d-inline-block"></span>
+                <a href="#" class=""><span class="mr-2  icon-phone"></span> <span class="d-none d-md-inline-block"> (+57604) 567 9101</span></a>
+
+
+                <div class="float-right">
+
+                    <a href="#" class=""><span class="mr-2  icon-twitter"></span> <span class="d-none d-md-inline-block">Twitter</span></a>
+                    <span class="mx-md-2 d-inline-block"></span>
+                    <a href="#" class=""><span class="mr-2  icon-facebook"></span> <span class="d-none d-md-inline-block">Facebook</span></a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+    <div class="container">
+        <div class="row align-items-center position-relative">
+
+
+            <div class="site-logo">
+                <a href="index.html" class="text-black"><span class="text-primary">Brand</a>
+            </div>
+
+            <div class="col-12">
+                <nav class="site-navigation text-right ml-auto " role="navigation">
+
+                    <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                        <li><a href="#home-section" class="nav-link">Inicio</a></li>
+                        <li><a href="#services-section" class="nav-link">Servicios</a></li>
+
+
+                        <li class="has-children">
+                            <a href="#about-section" class="nav-link">Conocenos</a>
+                            <ul class="dropdown arrow-top">
+                                <li><a href="#team-section" class="nav-link">Equipo</a></li>
+                                <li><a href="#pricing-section" class="nav-link">Cotizaciones</a></li>
+                                <li><a href="#faq-section" class="nav-link">Preguntas frecuentes</a></li>
+                                <li class="has-children">
+                                    <a href="#">Sobre nosotros</a>
+                                    <ul class="dropdown">
+                                        <li><a href="#">Historia</a></li>
+                                        <li><a href="#">Misión</a></li>
+                                        <li><a href="#">Visión</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li><a href="#why-us-section" class="nav-link">Por que elejirnos</a></li>
+
+                        <li><a href="#testimonials-section" class="nav-link">Testimonios</a></li>
+                        <li><a href="#blog-section" class="nav-link">Blog</a></li>
+                        <li><a href="#contact-section" class="nav-link">Contactenos</a></li>
+                    </ul>
+                </nav>
+
+            </div>
+
+            <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+    </div>
+
+</header>
