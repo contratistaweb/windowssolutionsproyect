97434bd534f6d53b33c6670da9d94d51ea8d3c8b
York Mosquera <yolemos_10@hotmail.com>
2021-09-19 21:51:42 -0500

    realizacion de formulario de contacto

--- views/pages/contact.php
+++ views/pages/contact.php
@@ -1,3 +1,57 @@
-<h1>Pagina de contacto</h1>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+<link rel="stylesheet" href="css/stylescontac.css">
 
-<h1>oeeee llave</h1>
+
+    <div id="imagen">
+        <img src="images/contactenos.png" alt="contáctenos" id="imag">
+    </div>
+
+    <p id="mensaje">Estimado cliente... En el siguiente formulario deposite los datos correspondientes
+        para tener el gusto de atenderlo
+    </p>
+
+        
+    <section>
+        <form action="">
+            <div class="row">
+                <div class="col-md-6 elemento">
+                    <label for="inputPassword5" class="form-label label">Nombres</label>
+                    <input type="text" id="nombre" name="nombre" class="form-control">
+                </div>
+                <div class="col-md-6 elemento">
+                    <label for="inputPassword5" class="form-label label">Apellidos</label>
+                    <input type="text" id="Apellido" name="Apellido" class="form-control">
+                </div>
+            </div>
+            <div class="inputmail mt-2">
+                <label id="labelcorreo" for="inputPassword5" class="form-label label">Correo Electrónico</label>
+                <input type="email" id="Correo" name="Correo" class="form-control">
+            </div>
+            <div class="row">
+                <div class="col-md-6 elemento mt-2">
+                    <label for="inputPassword5" class="form-label label">Ciudad</label>
+                    <input type="text" id="Ciudad" name="Ciudad" class="form-control">
+                </div>
+                <div class="col-md-6 elemento mt-2">
+                    <label for="inputPassword5" class="form-label label">Dirección</label>
+                    <input type="text" id="Dirección" name="Dirección" class="form-control">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 elemento mt-2">
+                    <label for="inputPassword5" class="form-label label">Celular</label>
+                    <input type="text" id="Celular" name="Celular" class="form-control">
+                </div>
+                <div class="col-md-6 elemento mt-2">
+                    <label for="inputPassword5" class="form-label label">Teléfono</label>
+                    <input type="text" id="Teléfono" name="Teléfono" class="form-control">
+                </div>
+            </div>
+            <div class="form-floating mt-2">
+                <label id="textarea" class="label" for="floatingTextarea2">Comentarios</label>
+                <textarea class="form-control text" placeholder="Deja un comentario aqui" id="floatingTextarea2" style="height: 100px"></textarea>
+            </div>
+        </form>
+    </section>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
