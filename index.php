d299e8de7f4f9c8568dbc04370fc87da0ba944df
Luillin A. Escobar <luillin.escobar@hotmail.com>
2021-10-18 23:15:24 -0500

    Implementacion metodo GET usuarios dashboard

--- index.php
+++ index.php
@@ -1,52 +1,114 @@
 ﻿<!DOCTYPE html>
 <html lang="en">
+<?php
+include_once "./controller/pages.controller.php";
+include_once "./controller/admin.controller.php";
 
-<head>
-    <meta charset="UTF-8">
-    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
-    <link rel="stylesheet" href="assets/fonts/icomoon/style.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/stylescontac.css">
-    <link rel="stylesheet" href="assets/css/awl.carousel.min.css">
-    <link rel="stylesheet" href="assets/css/gallery-grid.css">
+$pages = new PagesControlller();
+$admin = new AdminController();
 
-</head>
+if (isset($_GET['pag'])) {
+?>
+    <!-- pages -->
 
-<body>
-    <?php
-    include_once "./controller/pages.controller.php";
-    include_once "./controller/admin.controller.php";
+    <head>
+        <meta charset="UTF-8">
+        <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+        <link rel="stylesheet" href="assets/fonts/icomoon/style.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/stylescontac.css">
+        <link rel="stylesheet" href="assets/css/awl.carousel.min.css">
+        <link rel="stylesheet" href="assets/css/gallery-grid.css">
+    </head>
 
-    $pages = new PagesControlller();
-    $admin = new AdminController();
+    <body>
+        <!-- pages -->
 
-    if (isset($_GET['pag'])) {
+        <?php
         $pages->Index('./views/');
-    } else {
-        if (!isset($_GET['admin'])) {
-            $pages->Index('./views/');
-        } else {
-            $admin->Index('./views/');
-        }
-    }
+        ?>
+        <script src="assets/js/jquery-3.3.1.min.js"></script>
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.sticky.js"></script>
+        <script src="assets/js/main.js"></script>
+        <script src="assets/js/owl.carousel.js"></script>
+        <script src="https://kit.fontawesome.com/67c6ce199d.js" crossorigin="anonymous"></script>
+        <!-- Contact Page  -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+        <script>
+            baguetteBox.run('.tz-gallery');
+        </script>
+    </body>
+    <?php
+} else {
+    if (!isset($_GET['admin'])) {
     ?>
-    <script src="assets/js/jquery-3.3.1.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/jquery.sticky.js"></script>
-    <script src="assets/js/main.js"></script>
-    <script src="assets/js/owl.carousel.js"></script>
-    <script src="https://kit.fontawesome.com/67c6ce199d.js" crossorigin="anonymous"></script>
-    <!-- Contact Page  -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
-    <script>
-        baguetteBox.run('.tz-gallery');
-    </script>
-</body>
+        <!-- pages -->
+
+        <head>
+            <meta charset="UTF-8">
+            <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+            <link rel="stylesheet" href="assets/fonts/icomoon/style.css">
+            <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+            <link rel="stylesheet" href="assets/css/style.css">
+            <link rel="stylesheet" href="assets/css/stylescontac.css">
+            <link rel="stylesheet" href="assets/css/awl.carousel.min.css">
+            <link rel="stylesheet" href="assets/css/gallery-grid.css">
+        </head>
+
+        <body>
+            <!-- pages -->
+            <?php $pages->Index('./views/'); ?>
+            <script src="assets/js/jquery-3.3.1.min.js"></script>
+            <script src="assets/js/popper.min.js"></script>
+            <script src="assets/js/bootstrap.min.js"></script>
+            <script src="assets/js/jquery.sticky.js"></script>
+            <script src="assets/js/main.js"></script>
+            <script src="assets/js/owl.carousel.js"></script>
+            <script src="https://kit.fontawesome.com/67c6ce199d.js" crossorigin="anonymous"></script>
+            <!-- Contact Page  -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+            <script>
+                baguetteBox.run('.tz-gallery');
+            </script>
+        </body>
+    <?php
+    } else {
+    ?>
+        <!-- admin  -->
+
+        <head>
+            <meta charset="UTF-8">
+            <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <link rel="stylesheet" href="assets/css/adminStyles.css">
+            <title>Windows Solutions | Administacion del sitio</title>
+        </head>
+
+        <body>
+            <!-- admin  -->
+
+            <?php $admin->Index('./views/'); ?>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+            <script src="https://kit.fontawesome.com/67c6ce199d.js" crossorigin="anonymous"></script>
+            <script src="/assets/js/app.js"></script>
+        </body>
+<?php
+    }
+}
+?>
 
 </html>
--- views/admin/components/users/index.php
+++ views/admin/components/users/index.php
@@ -1,1 +1,51 @@
 <h1>Admin users</h1>
+<?php
+
+// if (!isset($_SESSION["validarSesion"])) {
+//     echo '<script>window.location = "index.php?pagina=ingreso"; </script>';
+//     return;
+// } else {
+//     if ($_SESSION["validarSesion"] != "ok") {
+//         echo '<script>window.location = "index.php?pagina=ingreso"; </script>';
+//         return;
+//     }
+// }
+
+$usuarios = UsersController::ctrSeleccionarRegistros();
+
+?>
+<table class="table table-striped">
+    <thead class="thead-dark ">
+        <tr>
+            <th scope="row">#</th>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Celular</th>
+            <th>Direccion</th>
+            <th>Role</th>
+            <th>Comentarios</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($usuarios as $key => $value) { ?>
+            <tr>
+                <th scope="row"><?php echo ($key + 1); ?></th>
+                <td><?php echo $value["id_usuario"]; ?></td>
+                <td><?php echo $value["nombre_usuario"]; ?></td>
+                <td><?php echo $value["email_usuario"]; ?></td>
+                <td><?php echo $value["celular_usuario"]; ?></td>
+                <td><?php echo $value["direccion_usuario"]; ?></td>
+                <td><?php echo $value["id_role"] == 1 ? 'Administrador' : ($value["id_role"] == 2 ? 'vendedor' : 'usuario'); ?></td>
+                <td><?php echo $value["comentario_usuario"]; ?></td>
+                <td>
+                    <div class="d-flex justify-content-between">
+                        <a name="update" id="update" class="btn btn-primary" role="button"><i class="fa fa-pencil"></i></a>
+                        <a name="delete" id="delete" class="btn btn-danger" role="button"><i class="fa fa-trash"></i></a>
+                    </div>
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
