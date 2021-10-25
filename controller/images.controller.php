e1fe4228f8b170c6d4610882c5f36bb7717a3a24
Andrey <andreyduque1987@gmail.com>
2021-10-25 14:37:10 -0500

    Inicio_CRUD

--- controller/images.controller.php
+++ controller/images.controller.php
@@ -1,10 +1,29 @@
 <?php
+include_once 'database/connect.php';
+include_once "model/images.model.php";
 
 class ImagesController
 {
-
     public function Index()
     {
         include_once 'views/admin/components/images/index.php';
+    }
+
+    static public function ctrSeleccionarRegistros()
+    {
+        $table = 'imagenes';
+        $database = new Database();
+
+        $db = $database->getConnection();
+        $imagesModel = new Images_Model($db);
+
+        $respuesta = $imagesModel->getData($table);
+
+        return $respuesta;
+    }
+
+    static public function ctrEliminarRegistro()
+    {
+        # code...
     }
 }
