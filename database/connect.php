b89ce258c0529f4c2923b9e9b4dc04daa1e60f47
Luillin A. Escobar <luillin.escobar@hotmail.com>
2021-11-27 17:15:54 -0500

    products error solved

--- database/connect.php
+++ database/connect.php
@@ -7,26 +7,24 @@ class Database
     {
         $this->db = null;
         try {
-           $this->db = new mysqli('localhost', 'windowss_wsdb', 'WindowsSolutions17', 'windowss_wsdb');
-           //$this->db = new mysqli('localhost', 'root', '', 'windowss_wsdb');
+            $this->db = new mysqli('localhost', 'windowss_wsdb', 'WindowsSolutions17', 'windowss_wsdb');
         } catch (Exception $e) {
             echo "No hay conexión con la base de datos: " . $e->getMessage();
         }
         return $this->db;
     }
 
-    function conectar (){
-        try {
-            $dbh = new PDO('mysql:host=localhost;dbname=windowss_wsdb', "WindowsSolutions17", "windowss_wsdb");
-           
-            return $dbh ;
-        } catch (PDOException $e) {
-            print "Error!: " . $e->getMessage() . "<br/>";
-            die();
-        }
-    }
+    // function conectar()
+    // {
+    //     try {
+    //         $dbh = new PDO('mysql:host=localhost;dbname=windowss_wsdb', "WindowsSolutions17", "windowss_wsdb");
 
-    
+    //         return $dbh;
+    //     } catch (PDOException $e) {
+    //         print "Error!: " . $e->getMessage() . "<br/>";
+    //         die();
+    //     }
+    // }
 }
 
 // class Conexion
